bbda2e2248d1883485dd6b2d1b0a3caaf99d7c9a
Zachary Bedrosian <zachary@cleancult.com>
2022-12-06 12:02:20 -0500

    Apple menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,76 @@ function MenuLink({ link }: { link: string }) {
   );
 }
 
+function TopMenu() {
+  return (
+    <Box component="header" bg="black">
+      <Container size={1024}>
+        <Flex align="center" justify="space-between" py={0}>
+          <Image
+            width={14}
+            src="https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__b5er5ngrzxqq_large.svg"
+            alt="Apple Logo"
+            sx={{
+              opacity: 0.7,
+              cursor: "pointer",
+              transition: "opacity 0.2s",
+              "&:hover": {
+                opacity: 1,
+              },
+            }}
+          />
+          <MenuLink link="Store" />
+          <MenuLink link="Mac" />
+          <MenuLink link="iPad" />
+          <MenuLink link="iPhone" />
+          <MenuLink link="Watch" />
+          <MenuLink link="AirPods" />
+          <MenuLink link="TV & Home" />
+          <MenuLink link="Only on Apple" />
+          <MenuLink link="Accesories" />
+          <MenuLink link="Support" />
+          <Box
+            px="xs"
+            sx={{
+              backgroundImage:
+                "url(https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_search_image__cbllq1gkias2_large.svg)",
+              width: "15px",
+              backgroundRepeat: "no-repeat",
+
+              backgroundSize: "15px 88px",
+              height: "44px",
+              opacity: 0.7,
+              cursor: "pointer",
+              transition: "opacity 0.2s",
+              "&:hover": {
+                opacity: 1,
+              },
+            }}
+          />
+          <Box
+            px="xs"
+            sx={{
+              backgroundImage:
+                "url(https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_bag_image__yzte50i47ciu_large.svg)",
+              width: "15px",
+              backgroundRepeat: "no-repeat",
+
+              backgroundSize: "13px 44px",
+              height: "44px",
+              opacity: 0.7,
+              cursor: "pointer",
+              transition: "opacity 0.2s",
+              "&:hover": {
+                opacity: 1,
+              },
+            }}
+          />
+        </Flex>
+      </Container>
+    </Box>
+  );
+}
+
 export default function Home() {
   return (
     <Box>
@@ -37,31 +107,7 @@ export default function Home() {
         title="Apple Card - Monthly Installments"
         description="A short description goes here."
       />
-      <Box component="header" bg="black">
-        <Container size={1024}>
-          <Flex align="center" justify="space-between" py={9}>
-            <Image
-              width={17}
-              height={27}
-              src="https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__b5er5ngrzxqq_large.svg"
-              alt="Apple Logo"
-              sx={{
-                opacity: 0.7,
-              }}
-            />
-            <MenuLink link="Store" />
-            <MenuLink link="Mac" />
-            <MenuLink link="iPad" />
-            <MenuLink link="iPhone" />
-            <MenuLink link="Watch" />
-            <MenuLink link="AirPods" />
-            <MenuLink link="TV & Home" />
-            <MenuLink link="Only on Apple" />
-            <MenuLink link="Accesories" />
-            <MenuLink link="Support" />
-          </Flex>
-        </Container>
-      </Box>
+      <TopMenu />
     </Box>
   );
 }
